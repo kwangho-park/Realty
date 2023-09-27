248a492793a2d311083782352f9ff1ec2fa3bc97
pkh <kwangho410@dreamsecurity.com>
2023-09-27 23:19:48 +0900

    [init] 배치 초기 프로젝트 생성
    1. 스팩
    - Spring boot batch, JPA, Gradle,
    
    2. 로직
    - reader : csv 파일, processor : 대문자 변환 , writer : DB
    
    3. 기타
    - jar 배포를 위한 MANIFEST 파일 추가, ER 다이어그램 / script 추가

---
+++ Batch/src/main/java/com/example/batchInitalizr/PersonItemProcessor.java
@@ -0,0 +1,27 @@
+package com.example.batchInitalizr;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+// 배치 비즈니스 로직
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+	private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+	@Override
+	public Person process(final Person person) throws Exception {
+
+		System.out.println("process test");
+
+		final String firstName = person.getFirstName().toUpperCase();
+		final String lastName = person.getLastName().toUpperCase();
+
+		final Person transformedPerson = new Person(firstName, lastName);
+
+		log.info("Converting (" + person + ") into (" + transformedPerson + ")");
+
+		return transformedPerson;
+	}
+
+}
