cf40c0756792b1164501c0206b90fc4fcff790df
kwang ho park <kwangho410@gmail.com>
2024-09-28 12:48:43 +0900

    [DB] 평단가 테스트를위한 더미데이터 추가

--- DataBase/latest/Realty_insert_data.sql
+++ DataBase/latest/Realty_insert_data.sql
@@ -31,3 +31,16 @@ values
 (4150000000,'경기도 이천시'),(4155000000,'경기도 안성시'),(4157000000,'경기도 김포시'),(4159000000,'경기도 화성시'),(4161000000,'경기도 광주시'),
 (4163000000,'경기도 양주시'),(4165000000,'경기도 포천시'),(4167000000,'경기도 여주시'),(4180000000,'경기도 연천군'),(4182000000,'경기도 가평군'),
 (4183000000,'경기도 양평군');
+
+
+
+-- 아파트 단지별 평단가 구현용 더미데이터 
+insert into tb_apt_building(AB_PNU, AB_NAME, AB_ADDRESS, AB_ROAD_ADDRESS) values('4119210800110510000','설악마을아파트(설악단지)','경기도 부천시 원미구 중동 1051번지','경기도 부천시 원미구 계남로 195');
+insert into tb_apt_building(AB_PNU, AB_NAME, AB_ADDRESS, AB_ROAD_ADDRESS) values('4119210800113000000','센트럴파크푸르지오','','부천시 원미구 소향로 181');
+insert into tb_apt_building(AB_PNU, AB_NAME, AB_ADDRESS, AB_ROAD_ADDRESS) values('4119210800113010000','힐스테이트중동','','부천시 원미구 길주로 234');
+insert into tb_apt_building(AB_PNU, AB_NAME, AB_ADDRESS, AB_ROAD_ADDRESS) values('4119210800110390000','은하마을(주공1)','','부천시 원미구 중동로 301');
+insert into tb_apt_building(AB_PNU, AB_NAME, AB_ADDRESS, AB_ROAD_ADDRESS) values('4119210800111700000','포도마을(삼보)','','부천시 원미구 조마루로 135');
+
+-- 설악마을 평형별 면적 
+insert into tb_area_type(AT_PRIVATE_AREA, AT_PUBLIC_AREA, AT_SUPPLY_AREA, AT_AB_PNU) values('44.1','16.41','60.51','4119210800110510000');
+insert into tb_area_type(AT_PRIVATE_AREA, AT_PUBLIC_AREA, AT_SUPPLY_AREA, AT_AB_PNU) values('49.8','19.65','69.45','4119210800110510000');
